e7cefd4feb39f15b825b96d872b52d81645e3f91
FranvTapia <franv.tapia@gmail.com>
2023-12-17 19:22:20 -0300

    Pequeño analisis y base actualizada

--- Analisis_mediciones.R
+++ Analisis_mediciones.R
@@ -1,45 +1,29 @@
 
-base_mediciones <- Base
+base_mediciones <- Base_pacientes
 
+# Convertir la columna FECHA a formato de fecha
+base_mediciones$FECHA <- as.Date(base_mediciones$FECHA, format="%d/%m/%Y")
 
-#Estadistica descriptiva mediciones noche
-Est_desc <- base_mediciones %>% 
-  group_by(ID) %>% 
-  summarise(M_mean=mean(MEDICION_MANANA,na.rm = T),
-            M_Min=min(MEDICION_MANANA,na.rm = T),
-            M_Max=max(MEDICION_MANANA,na.rm = T),
-            M_desv = sd(MEDICION_MANANA,na.rm = T),
-            N_Mean=mean(MEDICION_NOCHE,na.rm = T),
-            N_Min=min(MEDICION_NOCHE,na.rm = T),
-            N_Max=max(MEDICION_NOCHE,na.rm = T),
-            N_desv = sd(MEDICION_NOCHE,na.rm = T),
-            Dif_Promedio = mean(abs(MEDICION_MANANA - MEDICION_NOCHE), na.rm = TRUE))
+# Tendencia de glucosa en la mañana
+ggplot(base_mediciones, aes(x=FECHA, y=MEDICION_MANANA, color=factor(ID))) +
+  geom_line() +
+  labs(title="Tendencia de Glucosa en la Mañana a lo largo del tiempo", x="Fecha", y="Medición de Glucosa") +
+  theme_minimal()
 
-#Las mediciones de la mañana están más dispersas que las de la noche
-#En la mañana la medicion más alta fue de 231 (p3), y la más baja de 97 (p2)
-#En la noche la medicion más alta fue de 200 (p3), y la más baja de 110 (p2)
-#Tanto  en la mañana como en la noche el paciente 3 tiene mediciones mas dispersas
-# También tiene la mayor dif promedio entre la mañana y la noche
+# Tendencia de glucosa en la noche
+ggplot(base_mediciones, aes(x=FECHA, y=MEDICION_NOCHE, color=factor(ID))) +
+  geom_line() +
+  labs(title="Tendencia de Glucosa en la Noche a lo largo del tiempo", x="Fecha", y="Medición de Glucosa") +
+  theme_minimal()
 
+#Box plot comparación por genero
+dia  <- ggplot(base_mediciones, aes(x=factor(SEXO), y=MEDICION_MANANA)) +
+  geom_boxplot() +
+  labs(title="Comparación de Glucosa en la Mañana por Género", x="Género", y="Medición de Glucosa") +
+  theme_minimal()
+noche <- ggplot(base_mediciones, aes(x=factor(SEXO), y=MEDICION_NOCHE)) +
+  geom_boxplot() +
+  labs(title="Comparación de Glucosa en la Noche por Género", x="Género", y="Medición de Glucosa") +
+  theme_minimal()
 
-#Grafico comparativo 1:
-
-datos_combinados <- bind_rows(
-  base_mediciones %>%
-    select(FECHA, MEDICION_MANANA, ID) %>%
-    mutate(Tipo = "Mañana"),
-  base_mediciones %>%
-    select(FECHA, MEDICION_NOCHE, ID) %>%
-    mutate(Tipo = "Noche")
-)
-
-ggplot(datos_combinados, aes(x = FECHA, y = ifelse(Tipo == "Mañana", MEDICION_MANANA, MEDICION_NOCHE), color = Tipo)) +
-  geom_point() +
-  labs(title = "Comaparacion entre los niveles de Glicemia en la mañana y noche",
-       x = "Fecha", y = "Nivel de Glicemia") +
-  facet_wrap(~ ID, scales = "free_x", ncol = 1) +
-  scale_color_manual(values = c("Mañana" = "orange", "Noche" = "blue"))
-
-#Las mediciones del paciente 3 se ven mucho más dispersas, lo que tiene sentido considerando lo alta
-#que fue su desviacion estandar y dif promedio entre la mañana y la noche
-
+grid.arrange(dia, noche, ncol = 2)
